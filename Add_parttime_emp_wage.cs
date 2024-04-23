39d449ec85316d7de5b5acddcbd5ab589f89487f
Thotajyothsna <thotajyothsna2001@gmail.com>
2024-04-23 11:45:46 +0530

    Adding part time employee and wage

---
+++ Add_parttime_emp_wage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee_wage
+{
+    class Add_parttime_emp_wage
+    {
+        int IS_FULL_TIME = 2;
+        int IS_PART_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
+        public void wage_1_day()
+        {
+
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 3);
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empWage = 4;
+            
+            }
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("EMP WAGE:{0}", empWage);
+        }
+    }
+}
