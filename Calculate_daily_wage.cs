19d952f7d3fbebf878ad712538c8b02f7ca53eee
Thotajyothsna <thotajyothsna2001@gmail.com>
2024-04-23 11:43:33 +0530

    calculate employee daily wage

---
+++ Calculate_daily_wage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee_wage
+{
+     class Calculate_daily_wage
+    {
+        int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
+        public void wage_1_day()
+        {
+
+            Random rand= new Random();
+            int empCheck=rand.Next(0,2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("EMP WAGE:{0}",empWage);
+        }
+    }
+}
